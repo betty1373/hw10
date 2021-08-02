4393df56ffa22c754dfb515b88c9b6e2850479a0
betty1373 <betty1373@mail.ru>
2021-08-02 16:56:29 +0300

    Bulk server

--- inc/Server.h
+++ inc/Server.h
@@ -22,18 +22,19 @@ private:
   std::shared_ptr<CmdReader> m_cmdReader;
   enum { max_length = 1024 };
   char data_[max_length];
+  std::string m_clientId;
+  std::stringstream m_strstream;
 }
 
 class Server
 {
 public:
-  Server(boost::asio::io_context& io_context, short port, std::size_t numCmds);
- 
+  Server(boost::asio::io_context& io_context, short port, std::size_t numCmds); 
 private:
   void Do_accept();
 
   tcp::acceptor m_acceptor;
-
+  tcp::socket m_socket;
   std::shared_ptr<CmdReader> m_cmdReader;
   std::shared_ptr<ConsoleLogger> m_consoleLogger;
   std::shared_ptr<FileLogger> m_fileLogger;
--- src/Server.cpp
+++ src/Server.cpp
@@ -2,20 +2,24 @@
 Session::Session(tcp::socket socket, std::shared_ptr<CmdReader>& reader)
     : m_socket(std::move(socket)),
       m_cmdReader(reader)
-  {
-  }
+{
+    auto endpoint = m_socket.remote_endpoint();
+    m_clientId = endpoint.address().to_string() + ":"+ std::to_string(endpoint.port());     
+}
 
-  void Session::Start()
-  {
-    do_read();
-  }
+void Session::Start()
+{ 
+    m_cmdReader->AddClient(m_clientId);
+    Do_read();
+}
 
-void Session::do_read()
+void Session::Do_read()
   {
     auto self(shared_from_this());
-    socket_.async_read_some(boost::asio::buffer(data_, max_length),
+    m_socket.async_read_some(boost::asio::buffer(data_, max_length),
         [this, self](boost::system::error_code ec, std::size_t length)
         {
+          
           if (!ec)
           {
             std::cout << "receive " << length << "=" << std::string{data_, length} << std::endl;
@@ -25,7 +29,8 @@ void Session::do_read()
   }
 
 Server::Server(boost::asio::io_context& io_context, short port, std::size_t numCmds)
-    : m_acceptor(io_context, tcp::endpoint(tcp::v4(), port))
+    : m_acceptor(io_context, tcp::endpoint(tcp::v4(), port)),
+    m_socket(io_context)
   {
     m_cmdReader =  CmdReader::Create(static_cast<size_t>(numCmds));
     m_consoleLogger = ConsoleLogger::Create("log",m_cmdReader);
@@ -33,16 +38,16 @@ Server::Server(boost::asio::io_context& io_context, short port, std::size_t numC
     Do_accept();
   }
 
-void Sever::Do_accept()
-  {
-    acceptor_.async_accept(
-        [this](boost::system::error_code ec, tcp::socket socket)
+void Server::Do_accept()
+{
+    m_acceptor.async_accept(m_socket,
+        [this](boost::system::error_code ec)
         {
           if (!ec)
           {
-            std::make_shared<Session>(std::move(socket))->Start();
+            std::make_shared<Session>(std::move(m_socket), m_cmdReader)->Start();
           }
 
           Do_accept();
         });
-  }
+}
